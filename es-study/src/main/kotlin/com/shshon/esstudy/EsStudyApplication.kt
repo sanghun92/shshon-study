824e44e06153afade51c6331d2d2f05abbe25dae
sanghun92 <mue0329@gmail.com>
2023-11-05 18:45:38 +0900

    [es-study] feat: elastic search 사이드 프로젝트 생성

---
+++ es-study/src/main/kotlin/com/shshon/esstudy/EsStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.shshon.esstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EsStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<EsStudyApplication>(*args)
+}
---
+++ es-study/src/test/kotlin/com/shshon/esstudy/EsStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shshon.esstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EsStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
